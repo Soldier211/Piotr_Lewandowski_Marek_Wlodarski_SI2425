4586d644a774de11753b0bd97a3e616716e969fa
MarekWlodarskii <102950684+MarekWlodarskii@users.noreply.github.com>
2025-11-26 18:38:37 +0100

    z2, można później dodać jakieś testy

---
+++ zad2.pl
@@ -0,0 +1,22 @@
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+parent(joe, jim).
+
+male(tom).
+male(bob).
+male(joe).
+male(jim).
+
+female(pam).
+female(liz).
+female(ann).
+female(pat).
+
+son(X, Y) :- parent(Y, X), male(X).
+granddaughter(X, Y) :- parent(Y, Z), parent(Z, X), female(X).
+aunt(X, Y) :- female(X), parent(Z, X), parent(Z, P), parent(P, Y), \+parent(X, Y).
+partner(X, Y) :- parent(X, Z), parent(Y, Z), X\=Y.
